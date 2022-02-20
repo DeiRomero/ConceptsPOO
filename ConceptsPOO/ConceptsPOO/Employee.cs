1e6e3acb8dcee2ac5b8ae347731af9302bb2acaf
DEI ROMERO <DEI ROMERO@DESKTOP-UNSED2L>
2022-02-20 18:47:07 -0500

    Finish class Employee, SalaryEmplye, CommissionEmploye

---
+++ ConceptsPOO/ConceptsPOO/Employee.cs
@@ -0,0 +1,31 @@
+﻿namespace ConceptsPOO
+{
+    public abstract class Employee
+    {
+        public int Id { get; set; }
+
+        public string FirsName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Date BisthDate { get; set; }
+
+        public Date HisthDate { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public abstract decimal GetValueToPay();
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, " +
+                $"Name: {FirsName}, " +
+                $"LastName: {LastName}, " +
+                $"Birth: {BisthDate}, " +
+                $"Hiring: {HisthDate}, " +
+                $"Is active: {IsActive},";
+        }
+
+
+    }
+}
--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -3,17 +3,34 @@
 Console.WriteLine("ConceptsPOO");
 Console.WriteLine("===========");
 
-try
+Employee employee1 = new SalaryEmployee()
 {
-    Console.WriteLine(new Date(2024, 2, 29));
-    Console.WriteLine(new Date(1974, 9, 23));
-    Console.WriteLine(new Date(1985, 11, 31));
+    Id = 1010,
+    FirsName = "Sandra",
+    LastName = "Morales",
+    BisthDate = new Date(1990, 5, 23),
+    HisthDate = new Date(2022, 1, 15),
+    IsActive = true,
+    Salary = 181545.45M
 
-}
-catch (Exception error)
+
+};
+
+Console.WriteLine(employee1);
+
+Employee employee2 = new CommissionEmployee()
 {
+    Id = 2020,
+    FirsName = "Patricia",
+    LastName = "Gutierrez",
+    BisthDate = new Date(1995, 5, 23),
+    HisthDate = new Date(2022, 2, 5),
+    IsActive = true,
+    Sales = 320000000M,
+    CommissionPercentaje = 0.03F    
 
-    Console.WriteLine(error.Message);
-}
 
+};
+
+Console.WriteLine(employee2);
 
